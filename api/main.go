5c8b00189e80909551986b1c082819b8133f32e6
Kim Juyoun <kimjuyoun@mobilus-imac-m41.local>
2019-05-23 18:34:07 +0900

    add framework echo serving react

--- api/main.go
+++ api/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	
+	"github.com/labstack/echo"
+)
 
-func main(){
-	fmt.Println("rofl")
+func main() {
+	e := echo.New()
+	e.GET("/test", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Static("/", "build/index.html")
+	e.Logger.Fatal(e.Start(":1323"))
 }

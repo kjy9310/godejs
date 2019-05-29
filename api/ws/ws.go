49226f00f970e178c2dd3b2dc3d3d444a7ca2ef2
Kim Juyoun <kimjuyoun@mobilus-imac-m41.local>
2019-05-29 18:50:24 +0900

    need to fix package problem

--- api/ws/ws.go
+++ api/ws/ws.go
@@ -1,21 +1,14 @@
-package main
+package ws
 
 import (
 	"fmt"
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
-	"net/http"
 	"strconv"
 	"time"
-	"./ws"
 )
 
-var (
-	upgrader = websocket.Upgrader{}
-)
-
-func hello(c echo.Context) error {
+func Listen(c echo.Context) error {
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
 		return err
@@ -37,24 +30,11 @@ func hello(c echo.Context) error {
 
 		fmt.Printf("%s\n", msg)
 		unixStampString := msg[0:10]
-		fmt.Println("unixStampString : ",string(unixStampString))
+		fmt.Println("unixStampString : ", string(unixStampString))
 		unixStamp, err := strconv.ParseInt(string(unixStampString), 10, 64)
-		if (err != nil){
+		if err != nil {
 			fmt.Println("!!!!ERR!!!!", err)
 		}
 		fmt.Println(time.Unix(int64(unixStamp), 0))
 	}
-}
-
-func main() {
-	e := echo.New()
-	e.GET("/test", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
-	e.Use(middleware.CORS())
-	e.GET("test2", ws.Listen())
-	e.File("/", "build/index.html")
-	e.Static("/static", "build/static")
-	e.GET("/ws", hello)
-	e.Logger.Fatal(e.Start(":1323"))
 }
